0d75757439a69861b5e575ae7472f8df0ef7404b
NIK0LADZE <82117945+NIK0LADZE@users.noreply.github.com>
2021-06-25 17:02:06 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="cache-control" content="no-cache"/>
+    <link rel="stylesheet" href="css/style.css">
+    <title>FixIT</title>
+</head>
+<body class="reset" id="scroll" onscroll="scrollSet()" onload="scrollSet()">
+<!-- Navigation -->
+<?php require_once("templates/nav.php"); ?>
+<!-- Navigation -->
+<section id="home-section">
+</section>
+<section id="prof-section">
+</section>
+<section id="exp-section"></section>
+<section id="about-section"></section>
+<section id="contact-section"></section>
+</body>
+</html>

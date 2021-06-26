bc0b9a9d6c8a30571b8f74de32ae3077a19ca22a
NIK0LADZE <82117945+NIK0LADZE@users.noreply.github.com>
2021-06-26 17:19:34 +0400

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="cache-control" content="no-cache"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="css/style.css">
     <title>FixIT</title>
 </head>

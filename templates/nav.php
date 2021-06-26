63b5d6b983c441dd0ab0d92685f63da9c8f3a9b0
NIK0LADZE <82117945+NIK0LADZE@users.noreply.github.com>
2021-06-26 17:18:42 +0400

    Update nav.php

--- templates/nav.php
+++ templates/nav.php
@@ -5,14 +5,14 @@ $menu = [
     ["name" => "გამოცდილება", "link" => "#exp-section", "id" => "menu-exp", "underline-id" => "exp-underline"],
     ["name" => "პროფესიონალი", "link" => "#prof-section", "id" => "menu-prof", "underline-id" => "prof-underline"],
     ["name" => "მთავარი", "link" => "#home-section", "id" => "menu-home", "underline-id" => "home-underline"]
-]; ?>
+]; 
+?>
 <nav class="navbar">
     <ul>
     <?php foreach ($menu as $item) { ?>
-    <li id="<?php echo $item['id'];?>"><a href="<?php echo $item['link'];?>" onmousedown="mouseDown('<?php echo $item['underline-id'];?>')" onmouseup="setTimeout(mouseUp, 1000)" onmouseover="removeId()" onmouseout="setId()"><?php echo $item['name'];?></a></li>
+    <li id="<?php echo $item['id'];?>"><a href="<?php echo $item['link'];?>" data-underline="<?php echo $item['underline-id'];?>" onmousedown="mouseDown('<?php echo $item['underline-id'];?>')" onmouseup="setTimeout(mouseUp, 1000)" onmouseover="removeId()" onmouseout="setId()"><?php echo $item['name'];?></a></li>
     <?php } ?>
     <div class="underline"></div>
     </ul>
 </nav>
-<?php require_once("nav-js.php");
-?>
+<script src="templates/nav.js" type="text/javascript"></script>
